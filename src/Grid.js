850dd69e9ac213352d062e1b6f272da7a5fa5dfe
“shivani.sah” <shivani.sah@ril.com>
2023-09-18 17:32:40 +0530

    refactored code

--- src/Grid.js
+++ src/Grid.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 import './Grid.css';
 
 const Grid = ({ numColumns, numBoxes }) => {
-  //const numRows = Math.ceil(numBoxes / numColumns);
   const gridRef = useRef(null);
 
   useEffect(() => {
@@ -18,12 +17,11 @@ const Grid = ({ numColumns, numBoxes }) => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.5, // Adjust this threshold as needed
+      threshold: 0.5,
     };
 
     const observer = new IntersectionObserver(handleIntersection, options);
 
-    // Get all the box elements and observe them
     const boxElements = document.querySelectorAll('.box');
     boxElements.forEach((box, index) => {
       box.dataset.boxIndex = index;
